1261d8d5a3375714937b594c36cc59630cf68bb1
Alexandre_oliveiradev <alexandredev.frontend@gmail.com>
2023-11-11 14:19:47 -0300

    a

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:4000",
-    headers: {
+/*     baseURL: "http://localhost:4000",
+ */    headers: {
         "Content-Type":"application/json"
-    }
-/*     baseURL: "https://espetomadeira-api.onrender.com",
- */})
+    },
+    baseURL: "https://espetomadeira-api.onrender.com",
+})
